b5198470a2101c6cf6570520aba2603d43cf1eac
WolfgangKonen <WolfgangKonen@users.noreply.github.com>
2025-11-17 10:26:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ from cx_Freeze import setup, Executable
 # File for creating standalone executable for oekolopoly 2.0
 # invoke: python setup.py build
 
-files_to_include = ['oekolopoly/my_oekolopoly_gui/assets', 'oekolopoly/my_oekolopoly_gui/trained_agents',
-                    'oekolopoly/my_oekolopoly_gui/bin', 'oekolopoly/my_oekolopoly_gui/Spielanleitung.pdf',
-                    'oekolopoly/my_oekolopoly_gui/feedback.txt','oekolopoly/my_oekolopoly_gui/current_game_history.txt']
+files_to_include = ['assets', 'trained_agents',
+                    'bin', 'Spielanleitung.pdf',
+                    ]
 
 packages_to_include = ['oekolopoly']
 
 executable = Executable(
-    script="oekolopoly/my_oekolopoly_gui/oekolopoly_gui.py",
+    script="oekolopoly_gui.py",
     base="gui",
     icon="oekolopoly/my_oekolopoly_gui/assets/python_icon.ico"
 )
 
 setup(
-    name="Oekolopoly_v1",
+    name="Oekolopoly_v2",
     version="1.0",
     description="Oekolopoly",
     author="Me",
